b53baa68751e26532ea69c83c27c1378b89dce22
William Huera <whuera@gmail.com>
2017-10-17 14:10:22 -0500

    unit test

--- predictorapp-core/src/main/test/com/app/predictor/test/BussinesRuleTest.java
+++ predictorapp-core/src/main/test/com/app/predictor/test/BussinesRuleTest.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.app.predictor.utils.Constants;
+
 public class BussinesRuleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetCalculateHours() {		
+		String message = Constants.MESSAGE_PREDICTOR_DEFAULT;
+		assertEquals("El vehiculo puede circular libremente sin restriccion",message);
+	}
+
+	@Test
+	public void testDayEnabled() {
+		String message = Constants.MESSAGE_PREDICTOR_DEFAULT;
+		assertEquals("El vehiculo puede circular libremente sin restriccion",message);
 	}
 
 }
