bb7bc972cd42385887a1c3d5d27b588bee84eec0
William Huera <whuera@gmail.com>
2017-10-17 12:30:35 -0500

    update component

---
+++ predictorapp-core/src/main/test/com/app/predictor/test/BussinesRuleTest.java
@@ -0,0 +1,14 @@
+package com.app.predictor.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BussinesRuleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
